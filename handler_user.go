0a414fa87dff751a93fdc80f20e4550aa33eacf2
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2025-11-22 20:03:32 +0800

    feat: create register cliCommand
    - Add required uuid Go dep.
    - Stores newly created user in db w/ CreateUser()
    - Updated the config object / json file with newly created username

--- handler_user.go
+++ handler_user.go
@@ -1,6 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/corygyarmathy/gator/internal/database"
+	"github.com/google/uuid"
+	"github.com/lib/pq"
+)
 
 func handlerLogin(s *state, cmd command) error {
 	if len(cmd.Args) == 0 {
@@ -15,4 +24,39 @@ func handlerLogin(s *state, cmd command) error {
 	fmt.Printf("Username has been set: %v", s.cfg.CurrentUserName)
 
 	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %v <name>", cmd.Name)
+	}
+	name := cmd.Args[0]
+
+	user, err := s.db.CreateUser(context.Background(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      name,
+	})
+	if err != nil {
+		var pqErr *pq.Error
+		if errors.As(err, &pqErr) && pqErr.Code == "23505" { // 23505: unique_violation
+			return fmt.Errorf("user '%v' already exists", name)
+		}
+		return fmt.Errorf("creating user in DB: %v", err)
+	}
+
+	err = s.cfg.SetUser(user.Name)
+	if err != nil {
+		return fmt.Errorf("setting username: %v", err)
+	}
+	fmt.Println("User has been created in DB")
+	printUser(user)
+
+	return nil
+}
+
+func printUser(user database.User) {
+	fmt.Printf(" * ID:      %v\n", user.ID)
+	fmt.Printf(" * Name:    %v\n", user.Name)
 }
--- main.go
+++ main.go
@@ -40,6 +40,10 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to register command: %v\n", err)
 	}
+	err = cmds.register("register", handlerRegister)
+	if err != nil {
+		log.Fatalf("Failed to register command: %v\n", err)
+	}
 
 	args := os.Args
 	if len(args) < 2 {
