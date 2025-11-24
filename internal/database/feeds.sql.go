caa9be61ffd9f3272960c2daaa1d5fb48d54ddac
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2025-11-24 14:03:58 +0800

    feat(db): add getFeedByUrl query

--- internal/database/feeds.sql.go
+++ internal/database/feeds.sql.go
@@ -14,14 +14,7 @@ import (
 
 const createFeed = `-- name: CreateFeed :one
 INSERT INTO feeds (id, created_at, updated_at, name, url, user_id)
-VALUES (
-    $1,
-    $2,
-    $3,
-    $4,
-    $5,
-    $6
-)
+VALUES ( $1, $2, $3, $4, $5, $6)
 RETURNING id, created_at, updated_at, name, url, user_id
 `
 
@@ -61,6 +54,24 @@ SELECT id, created_at, updated_at, name, url, user_id FROM feeds WHERE feeds.nam
 
 func (q *Queries) GetFeed(ctx context.Context, name string) (Feed, error) {
 	row := q.db.QueryRowContext(ctx, getFeed, name)
+	var i Feed
+	err := row.Scan(
+		&i.ID,
+		&i.CreatedAt,
+		&i.UpdatedAt,
+		&i.Name,
+		&i.Url,
+		&i.UserID,
+	)
+	return i, err
+}
+
+const getFeedByUrl = `-- name: GetFeedByUrl :one
+SELECT id, created_at, updated_at, name, url, user_id FROM feeds WHERE feeds.url = $1
+`
+
+func (q *Queries) GetFeedByUrl(ctx context.Context, url string) (Feed, error) {
+	row := q.db.QueryRowContext(ctx, getFeedByUrl, url)
 	var i Feed
 	err := row.Scan(
 		&i.ID,
--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -1,17 +1,13 @@
 -- name: CreateFeed :one
 INSERT INTO feeds (id, created_at, updated_at, name, url, user_id)
-VALUES (
-    $1,
-    $2,
-    $3,
-    $4,
-    $5,
-    $6
-)
+VALUES ( $1, $2, $3, $4, $5, $6)
 RETURNING *;
 
 -- name: GetFeed :one
 SELECT * FROM feeds WHERE feeds.name = $1;
+
+-- name: GetFeedByUrl :one
+SELECT * FROM feeds WHERE feeds.url = $1;
 
 -- name: ResetFeeds :exec
 DELETE FROM feeds;
