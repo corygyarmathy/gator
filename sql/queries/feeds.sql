1cfad466280464fed695327295806ddf49e4adc9
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2025-11-24 19:46:05 +0800

    feat(db): add MarkFeedFetched & GetNectFeedToFetch queries

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -14,3 +14,14 @@ DELETE FROM feeds;
 
 -- name: GetFeeds :many
 SELECT * FROM feeds;
+
+-- name: MarkFeedFetched :exec
+UPDATE feeds
+SET last_fetched_at = NOW(), updated_at = NOW()
+WHERE id = $1;
+
+
+-- name: GetNextFeedToFetch :one
+SELECT * FROM feeds
+ORDER BY last_fetched_at ASC NULLS FIRST
+LIMIT 1;
