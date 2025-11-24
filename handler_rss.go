3aa38608fc432144c0348fef57454bf39d850d6c
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2025-11-24 20:35:49 +0800

    feat: save scraped posts to db

--- handler_rss.go
+++ handler_rss.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"time"
 
+	"github.com/corygyarmathy/gator/internal/database"
 	"github.com/corygyarmathy/gator/internal/rssgator"
+	"github.com/google/uuid"
+	"github.com/lib/pq"
 )
 
 func handlerAggregator(s *state, cmd command) error {
@@ -44,7 +48,31 @@ func scrapeFeeds(s *state) error {
 		return fmt.Errorf("marking feed as fetched: %v", err)
 	}
 
-	printFeedItems(feed)
+	fmt.Printf("\nSaving posts from: %v\n\n", feed.Channel.Title)
+	for _, item := range feed.Channel.Item {
+		pubDate, err := time.Parse(time.RFC1123Z, item.PubDate)
+		if err != nil {
+			return fmt.Errorf("parsing post '%v' date '%v': %v", item.Title, item.PubDate, err)
+		}
+		post, err := s.db.CreatePost(context.Background(), database.CreatePostParams{
+			ID:          uuid.New(),
+			CreatedAt:   time.Now().UTC(),
+			UpdatedAt:   time.Now().UTC(),
+			Title:       item.Title,
+			Url:         item.Link,
+			Description: item.Description,
+			PublishedAt: pubDate,
+			FeedID:      nextFeed.ID,
+		})
+		if err != nil {
+			var pqErr *pq.Error
+			if errors.As(err, &pqErr) && pqErr.Code == "23505" { // 23505: unique_violation
+				continue
+			}
+			return fmt.Errorf("creating user in DB: %v", err)
+		}
+		fmt.Printf("Stored post to db: %v \n", post.Title)
+	}
 
 	return nil
 }
